4efe0ab16dab7246cfb738777eb31de02075ae55
luanarmo <ang3l_morales800@live.com>
2023-07-13 17:39:19 -0600

    first commit

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from .serializer import UserSerializer
+from .models import User
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
